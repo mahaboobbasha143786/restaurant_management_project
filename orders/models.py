a7d9404f30bdd1b976e6cda797ada80e415d285f
Shaik Mahaboob Basha <shaikmahaboobbasha143786@gmail.com>
2025-09-27 01:37:53 +0530

    50888

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class OrderStatus(models.Model):
+    name = models.CharField(max_lenght = 50, unique = True)
+
+    def __str__(self):
+        return self.name
 # Create your models here.
