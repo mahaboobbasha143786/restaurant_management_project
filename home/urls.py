7ed2cf0d7439b94c03f9cceadc2ce67f678f8309
Shaik Mahaboob Basha <shaikmahaboobbasha143786@gmail.com>
2025-09-26 16:27:45 +0530

    50890

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
     path('categories/', MenuCategoryListView.as_view(), name = 'menu-categories'),
+    path('api/', include('home.urls')),
 ]
