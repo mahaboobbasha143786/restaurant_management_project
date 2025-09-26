f024284f919407dacf57f14ceabbd8c01ea39711
Shaik Mahaboob Basha <shaikmahaboobbasha143786@gmail.com>
2025-09-27 01:33:29 +0530

    50889

---
+++ orders/utils.py
@@ -0,0 +1,10 @@
+import string
+import secrets
+from . models import Coupon
+def generate_coupon_code(length = 10):
+    characters = ftring.ascii_uppercase + string.digits
+
+    while True:
+        code = ''.join(secrets.choice(characters)for _ in range())
+        if not Coupon.objects.filter(code=code).exist():
+            return code
