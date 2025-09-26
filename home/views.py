44386f2b5ade808cda42b931e11dc6f736b6c56e
Shaik Mahaboob Basha <shaikmahaboobbasha143786@gmail.com>
2025-09-26 16:24:23 +0530

    50890

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from .models import MenuCategory
+from .serializers import MenuCategorySerializer
 
 # Create your views here.
+class MenuCategoryList view(ListAPIView):
+    queyset = MenuCategory.objects.all()
+    serializer_c;ass = MenuCategorySerializer
+    
