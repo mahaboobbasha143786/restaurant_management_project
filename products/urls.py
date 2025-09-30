70668a552d0ecc52bc9a202b75af7bb6ddd93b15
Shaik Mahaboob Basha <shaikmahaboobbasha143786@gmail.com>
2025-10-01 00:36:10 +0530

    51847

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
+    path("menu-items/filter/", manu_items_by_xategory, name="menu_items_by_category"),
 ]
