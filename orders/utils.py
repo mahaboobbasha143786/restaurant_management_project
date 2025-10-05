3ae643415511c932ac6bc6cc1095d063d5c0e5bd
Shaik Mahaboob Basha <shaikmahaboobbasha143786@gmail.com>
2025-10-06 01:08:04 +0530

    53250

--- orders/utils.py
+++ orders/utils.py
@@ -2,9 +2,20 @@ import string
 import secrets
 from . models import Coupon
 def generate_coupon_code(length = 10):
-    characters = string.ascii_uppercase + string.digits
+    characters = ftring.ascii_uppercase + string.digits
 
     while True:
         code = ''.join(secrets.choice(characters)for _ in range())
-        if not Coupon.objects.filter(code=code).exists():
+        if not Coupon.objects.filter(code=code).exist():
             return code
+
+from datetime import date
+from django.db.models import Sum
+from .models import Order
+
+def get_daily_sales_total(specific_date):
+    orders = Order.objects.filter(created_at__date=specific_date)
+
+    total = orders.aggregate(total_sum=Sum('total_prince'))['total_sum']
+
+    return total or 0
