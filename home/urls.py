62d5580ec4e87e063ffd6950881106d86120cb24
Shaik Mahaboob Basha <shaikmahaboobbasha143786@gmail.com>
2025-10-06 00:56:59 +0530

    53252

--- home/urls.py
+++ home/urls.py
@@ -4,4 +4,5 @@ from .views import *
 urlpatterns = [
     path('categories/', MenuCategoryListView.as_view(), name = 'menu-categories'),
     path('api/', include('home.urls')),
+    path('api/tables/<int:pk>/', TableDetailView.as_view(),name='time-detail'),
 ]
