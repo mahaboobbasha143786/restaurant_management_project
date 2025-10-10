d3416a414bc423a8c5ece7adebf561be6b624258
Shaik Mahaboob Basha <shaikmahaboobbasha143786@gmail.com>
2025-10-10 18:26:47 +0530

    54265

--- home/models.py
+++ home/models.py
@@ -5,3 +5,15 @@ class MenuCategory(models.Model):
     name = models.CharField(max_length=100, unique = True)
     def __str__(self):
         return self.name
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    contact_number = models.CharField(max_length=15)
+    operating_days = models.CharField(
+        max_length=100,
+        help_text="Enter days separated by commas (e.g., Mon, Tue, Wed, Thu, Fri"
+    )
+
+    def __str__(self):
+        return self.name
