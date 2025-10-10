d357041800076c83f8e68ce87708a43bfcc03eb3
Shaik Mahaboob Basha <shaikmahaboobbasha143786@gmail.com>
2025-10-10 18:19:54 +0530

    54266

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("orders/history/", OrderHistoryView(), name="order-history"),
+    path("<int:id>/", OrderDetailView.as_view(), name='order-detail'),
     
 ]
