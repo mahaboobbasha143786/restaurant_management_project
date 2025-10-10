8dea6181d2ced4fbb603f734bfb414f8fdcaa8f8
Shaik Mahaboob Basha <shaikmahaboobbasha143786@gmail.com>
2025-10-11 01:13:02 +0530

    54262

--- home/models.py
+++ home/models.py
@@ -17,3 +17,11 @@ class Restaurant(models.Model):
 
     def __str__(self):
         return self.name
+
+class Table(models.Model):
+    table_number = models.IntegerField(unique=True)
+    capacity = models.IntegerField()
+    is_available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Table {self.table_namber} (Capacity: {self.capacity})"
