989542f69016adcacb56a436cffec90e6a6a52ec
Shaik Mahaboob Basha <shaikmahaboobbasha143786@gmail.com>
2025-10-10 18:43:47 +0530

    54264

--- account/views.py
+++ account/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import permissions
+from django.contrib.auth.models import User
+from rest_framework.generics import RetrieveUpdateAPIView
+from .serializers import UserUpdateSerializer
+
+class UserProfileView(RetrieveUpdateAPIView):
+    serializer_class = UserUpdateSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
 
 # Create your views here.
